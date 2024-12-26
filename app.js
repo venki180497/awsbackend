0ebc67fff6980c721362eb8e88c67ce7b8749a3d
venki180497 <vengat180497@gmail.com>
2024-12-26 12:55:49 +0530

    update

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const routes = require('./src/routes/index');
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next(); // Proceed to the next middleware/route handler
+});
 app.use('/', routes);
 const PORT = process.env.PORT || 4000;
 
