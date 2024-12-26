8b824fa58acbc51ce342361f3fbaf9c182d237db
venki180497 <vengat180497@gmail.com>
2024-12-26 12:17:57 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use('/', routes);
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
